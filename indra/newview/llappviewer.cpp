52a9f3b42ea7a67d0211b0bd60716d3aad4d9783
tasia <tasia@easierit.org>
2026-05-17 02:04:41 +0200

    Fix TasiaFeed crash: remove manual draw() call, add refreshControls(), safe null checks

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -242,7 +242,7 @@
 #include "llfloatersnapshot.h"
 #include "llfloaterflickr.h"
 #include "fsfloaterprimfeed.h" // <FS:Beq/> Primfeed Floater
-#include "llfloatertasiafeed.h"
+
 #include "llsidepanelinventory.h"
 
 // includes for idle() idleShutdown()
@@ -1793,7 +1793,7 @@ bool LLAppViewer::doFrame()
                     LLFloaterSimpleSnapshot::update();
                     LLFloaterFlickr::update(); // <FS:Beq/> FIRE-35002 - Flickr preview not updating whne opened directly from tool tray icon
                     FSFloaterPrimfeed::update(); // <FS:Beq/> Primfeed support
-                    LLFloaterTasiaFeed::update();
+
                     gGLActive = false;
                 }
 
