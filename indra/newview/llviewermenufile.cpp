72b838bf18eb6cb42b06846994c5b6a1766e9f0a
Tasia <tasia@easierit.org>
2026-06-19 13:33:05 +0200

    Fix clipboard upload status text update

--- indra/newview/llviewermenufile.cpp
+++ indra/newview/llviewermenufile.cpp
@@ -1027,7 +1027,7 @@ public:
 private:
     void setStatus(const std::string& message)
     {
-        childSetText("status_text", message);
+        getChild<LLUICtrl>("status_text")->setValue(message);
     }
 
     void onClose()
