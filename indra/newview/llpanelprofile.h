06f061b5cbc72385ea4555252f67cd4c4686e514
tasia <tasia@easierit.org>
2026-08-01 17:00:45 +0200

    Restore upstream mAllowEdit/setAllowEdit lost in badge header cherry-pick

--- indra/newview/llpanelprofile.h
+++ indra/newview/llpanelprofile.h
@@ -132,6 +132,8 @@ public:
 
     void processProperties(void* data, EAvatarProcessorType type) override;
 
+    void setAllowEdit(bool allow_edit) { mAllowEdit = allow_edit; }
+
     // <FS:Zi> FIRE-32184: Online/Offline status not working for non-friends
     void onAvatarProperties(const LLAvatarData* data);
 
@@ -297,6 +299,7 @@ private:
     bool                mAllowPublish;
     bool                mPreview; // <AS:Chanayane> Preview button
     bool                mHideAge;
+    bool                mAllowEdit;
     std::string         mDescriptionText;
     std::string         mOriginalDescriptionText; // <AS:Chanayane> Preview button
     LLUUID              mImageId;
